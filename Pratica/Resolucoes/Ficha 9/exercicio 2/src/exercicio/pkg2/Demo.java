64698f3dc016eab59c100a7cd427c162cc389e46
MoreiraJorge <moreirajorge_17@outlook.pt>
2019-04-18 17:46:05 +0100

    Added ex2 ficha9 classes

---
+++ Pratica/Resolucoes/Ficha 9/exercicio 2/src/exercicio/pkg2/Demo.java
@@ -0,0 +1,16 @@
+package exercicio.pkg2;
+
+/**
+ *
+ * @author Jorge Moreira
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

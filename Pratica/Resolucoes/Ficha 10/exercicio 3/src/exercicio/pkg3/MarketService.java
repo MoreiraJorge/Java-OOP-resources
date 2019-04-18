2463c02085f924bcf7765068622ad8274aed24eb
MoreiraJorge <moreirajorge_17@outlook.pt>
2019-04-18 11:12:44 +0100

    ex3 ficha 10

---
+++ Pratica/Resolucoes/Ficha 10/exercicio 3/src/exercicio/pkg3/MarketService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio.pkg3;
+
+/**
+ *
+ * @author Jorge Moreira
+ */
+public interface MarketService {
+    public double getPotatoesPrice();
+    public void setPotatoesPrice( double p);
+    public double getMarketTotal(double kilos);
+}

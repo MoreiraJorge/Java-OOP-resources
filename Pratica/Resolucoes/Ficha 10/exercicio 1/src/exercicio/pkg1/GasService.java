b5f86614882440cae45fe24abbb15c84d2089511
MoreiraJorge <moreirajorge_17@outlook.pt>
2019-04-18 10:19:07 +0100

    ex1 ficha 10

---
+++ Pratica/Resolucoes/Ficha 10/exercicio 1/src/exercicio/pkg1/GasService.java
@@ -0,0 +1,14 @@
+package exercicio.pkg1;
+
+/**
+ *
+ * @author Jorge Moreira
+ */
+public interface GasService {
+    
+    public double getGasPrice();
+    
+    public void setGasPrice(double p);
+    
+    public double getGasTotal(double litres);
+}

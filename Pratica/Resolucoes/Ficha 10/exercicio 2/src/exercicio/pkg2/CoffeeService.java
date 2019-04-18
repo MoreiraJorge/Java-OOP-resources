7766491e429a9dc300cf34f4e6c35f74fe2e6e7e
MoreiraJorge <moreirajorge_17@outlook.pt>
2019-04-18 10:44:38 +0100

    ex2 ficha 10

---
+++ Pratica/Resolucoes/Ficha 10/exercicio 2/src/exercicio/pkg2/CoffeeService.java
@@ -0,0 +1,12 @@
+package exercicio.pkg2;
+
+/**
+ *
+ * @author Jorge Moreira
+ */
+public interface CoffeeService {
+    
+    public double getCoffeePrice();
+    public void setCoffeePrice(double p);
+    public double getCoffeeTotal(int coffees);
+}
---
+++ Pratica/Resolucoes/Ficha 10/exercicio 2/src/exercicio/pkg2/GasService.java
@@ -0,0 +1,14 @@
+package exercicio.pkg2;
+
+/**
+ *
+ * @author Jorge Moreira
+ */
+public interface GasService {
+    
+    public double getGasPrice();
+    
+    public void setGasPrice(double p);
+    
+    public double getGasTotal(double litres);
+}

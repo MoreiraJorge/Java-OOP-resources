e69336efefd57c753ee923e5a8387f1df9bf1ea1
MoreiraJorge <moreirajorge_17@outlook.pt>
2019-05-24 18:54:57 +0100

    ficha 11 ex1 incomplete with javadoc

--- Pratica/Resolucoes/Ficha 11/exercicio1/src/Interfaces/AppFunctions.java
+++ Pratica/Resolucoes/Ficha 11/exercicio1/src/Interfaces/AppFunctions.java
@@ -5,15 +5,40 @@ import CustomExceptions.NonSuportedException;
 import pod.File;
 
 public interface AppFunctions {
-
+    /**
+     * Metodo para adicionar ficheiros
+     * @param file
+     * @throws NullPointerException
+     * @throws IndexOutOfBoundsException
+     * @throws FullMemoryException
+     */
     public void addfile(File file) throws NullPointerException, IndexOutOfBoundsException, FullMemoryException;
 
+    /**
+     * Metodo para remover ficheiros
+     * @param index
+     * @throws ArrayIndexOutOfBoundsException
+     * @throws NullPointerException
+     */
     public void delete(int index)throws ArrayIndexOutOfBoundsException, NullPointerException;
 
+    /**
+     * Metodo de escolher a musica
+     * @param index
+     * @throws NonSuportedException
+     * @throws ArrayIndexOutOfBoundsException
+     * @throws NullPointerException
+     */
     public void playTrack(int index) throws NonSuportedException, ArrayIndexOutOfBoundsException, NullPointerException;
 
+    /**
+     * Metodo de escolher a musica seguinte
+     */
     public void nextTrack();
 
+    /**
+     * metodo de escolher a musica anterior
+     */
     public void previousTrack();
 
 
